3d46109db01aec7b65697e0c0403c476618d22bf
Logan Kimball <logankimbs@gmail.com>
2021-03-20 16:44:52 -0600

    Updated

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="main">
+    <div class="contact-name">Logan Kimball</div>
+    <div class="contact-number">(111) 111-1111</div>
+    <button>Add</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: "ContactCard",
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
-<div>
-    Hello
+<div class="main">
+    <CreateContact />
+    <ContactCard />
 </div>
 </template>
 
 <script>
+import CreateContact from '../components/CreateContact.vue'
+import ContactCard from '../components/ContactCard.vue'
 
+export default {
+    name: 'Home',
+    components: {
+        CreateContact,
+        ContactCard
+    },
+}
 </script>
+
+<style>
+
+</style>
