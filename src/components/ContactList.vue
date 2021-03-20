9c5b9aad048dfc11c8eddaf2e49247db1a02a966
Logan Kimball <logankimbs@gmail.com>
2021-03-20 17:39:04 -0600

    Added remove funcinality

---
+++ src/components/ContactList.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+        <div v-for="contact in this.$root.$data.contacts" v-bind:key="contact.id">
+            <div>{{contact.first_name + " " + contact.last_name}}</div>
+            <div>{{contact.phone_number}}</div>
+            <td class="left">
+                <button @click="editContact(contact)">Edit</button>
+                <button @click="removeContact(contact)">Remove</button>
+            </td>
+        </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "ContactList",
+        methods: {
+            editContact(contact) {
+                
+            },
+
+            removeContact(contact) {
+                let list = this.$root.$data.contacts;
+                for (var i = 0; i < list.length; i++){
+                    if (list[i].id === contact.id) {
+                        list.splice(i, 1); 
+                        break;
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,9 +1,23 @@
 <template>
 <div>
-    Hi
+    <h1>Contacts</h1>
+    <hr>
+    <div v-if="this.$root.$data.contacts.length === 0">
+        <h2>Go explore or add some people to your contacts</h2>
+    </div>
+    <div v-else>
+        <ContactList />
+    </div>
 </div>
 </template>
 
 <script>
+import ContactList from '../components/ContactList.vue'
 
+export default {
+    name: "Contacts",
+    components: {
+        ContactList
+    }
+}
 </script>
