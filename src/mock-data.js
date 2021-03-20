b3f6b994569e6113dfcef7c000c7b949d8d0b05c
Logan Kimball <logankimbs@gmail.com>
2021-03-20 13:33:13 -0600

    updated

---
+++ src/mock-data.js
@@ -0,0 +1,16 @@
+let mock = [{
+    id: 1,
+    name: "Egg Salad",
+    price: "$5.62",
+    country: "United States",
+    image: 'egg-salad.jpg'
+},
+{
+    id: 2,
+    name: "Sweet Potato",
+    price: "$2.41",
+    country: "Brazil",
+    image: "sweet-potato.jpg"
+}]
+
+export default mock;
