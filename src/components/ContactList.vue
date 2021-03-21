e76a4930bc993f12bc75fdf4327c01d72c4c4f63
Logan Kimball <logankimbs@gmail.com>
2021-03-20 21:22:01 -0600

    Added styles to contacts page

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-        <div v-for="contact in this.$root.$data.contacts" v-bind:key="contact.id">
+    <div class="contacts">
+        <div class="cards">
+        <div class="card" v-for="contact in this.$root.$data.contacts" v-bind:key="contact.id">
             <div class="firstname" contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'first_name')">{{contact.first_name}}</div>
             <div class="lastname" contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'last_name')">{{contact.last_name}}</div>
             <div contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'email')">{{contact.email}}</div>
             <div contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'phone_number')">{{contact.phone_number}}</div>
             <button @click="removeContact(contact)">Remove</button>
+        </div>
         </div>
     </div>
 </template>
@@ -43,6 +45,73 @@
     }
 </script>
 
-<style>
+<style scoped>
+    .contacts {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .firstname,
+    .lastname {
+        font-weight: bold;
+        font-size: large;
+    }
+
+    .cards {
+        margin-top: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+    .card {
+        margin: 10px;
+        border-radius: 12px;
+        background-color: rgba(53, 53, 53, 0.055);
+        box-shadow: 5px 2px 5px rgba(0, 0, 0, 0.219);
+        width: 500px;
+        border: none;
+        padding: 20px 20px;
+    }
+
+    button {
+        margin-top: 12px;
+        padding: 5px 12px;
+        border-radius: 12px;
+        background-color: rgb(216, 91, 91);
+        font-size: large;
+        color: white;
+        border: none;
+    }
+
+    button:hover {
+        background-color: rgb(73, 73, 167);
+    }
+
+    .name {
+        font-size: large;
+        font-weight: bold;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    /* Mobile Styles */
+    @media only screen and (max-width: 767px) {
+        .card {
+            width: 90%;
+        }
+    }
+
+    /* Tablet Styles */
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+        
+    }
+
+    /* Desktop Styles */
+    @media only screen and (min-width: 1024px) {
+        
+    }
 </style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,11 +1,13 @@
 <template>
-<div>
+<div class="contacts">
     <h1>Contacts</h1>
-    <hr>
     <div v-if="this.$root.$data.contacts.length === 0">
         <h2>Go explore or add some people to your contacts</h2>
     </div>
     <div v-else>
+        <div class="protipCard">
+            <h3>Pro Tip: Click directly on contact info to edit than hit enter</h3>
+        </div>
         <ContactList />
     </div>
 </div>
@@ -21,3 +23,59 @@ export default {
     }
 }
 </script>
+
+<style>
+    .contacts {
+        margin-bottom: 80px;
+    }
+
+    h1, h2 {
+        text-align: center;
+        margin-top: 36px;
+        margin-bottom: 26px;
+    }
+
+    input,
+    .add {
+        width: 60%;
+        padding: 12px 18px;
+        border-radius: 12px;
+        border: solid black 1.8px;
+        margin-bottom: 6px;
+        font-size: large;
+    }
+
+    .add {
+        margin-top: 8px;
+        background-color: rgb(91, 91, 216);
+        font-size: large;
+        color: white;
+        border: none;
+    }
+
+    .add:hover {
+        background-color: rgb(73, 73, 167);
+    }
+
+    .protipCard {
+        text-align: center;
+        color: rgb(91, 91, 216);
+    }
+
+    /* Mobile Styles */
+    @media only screen and (max-width: 767px) {
+        input, .add {
+            width: 80%;
+        }
+    }
+  
+    /* Tablet Styles */
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+
+    }
+
+    /* Desktop Styles */
+    @media only screen and (min-width: 1024px) {
+        
+    }
+</style>
