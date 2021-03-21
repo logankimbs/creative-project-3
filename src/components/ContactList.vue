d1e100718e1076fa87ecc24046988ddd585398ff
Logan Kimball <logankimbs@gmail.com>
2021-03-20 20:04:52 -0600

    Added update functionality

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div v-for="contact in this.$root.$data.contacts" v-bind:key="contact.id">
-            <h2>{{contact.first_name + " " + contact.last_name}}</h2>
-            <h2>{{contact.email}}</h2>
-            <h2>{{contact.phone_number}}</h2>
-            <button>Edit</button>
+            <div class="firstname" contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'first_name')">{{contact.first_name}}</div>
+            <div class="lastname" contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'last_name')">{{contact.last_name}}</div>
+            <div contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'email')">{{contact.email}}</div>
+            <div contenteditable="true" v-on:keydown.enter="updateContact($event, contact, 'phone_number')">{{contact.phone_number}}</div>
             <button @click="removeContact(contact)">Remove</button>
         </div>
     </div>
@@ -14,6 +14,22 @@
     export default {
         name: "ContactList",
         methods: {
+            updateContact(e, contact, type) {
+                e.preventDefault();
+
+                if (type === 'email') {
+                    contact.email = e.target.innerText;
+                } else if (type === 'first_name') {
+                    contact.first_name = e.target.innerText;
+                } else if (type === 'last_name') {
+                    contact.last_name = e.target.innerText;
+                } else {
+                    contact.phone_number = e.target.innerText;
+                }
+
+                e.target.blur();
+            },
+
             removeContact(contact) {
                 let list = this.$root.$data.contacts;
                 for (var i = 0; i < list.length; i++){
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,13 @@
         <div class="container">
             <h1>Add Friend</h1>
             <form v-on:submit.prevent>
-                <input type="text" placeholder="First Name" id="fName" v-model="fName">
-                <input type="text" placeholder="Last Name" id="lName" v-model="lName">
+                <input type="text" placeholder="First Name" id="fName" v-model="fName"><br>
+                <input type="text" placeholder="Last Name" id="lName" v-model="lName"><br>
+                <input type="text" placeholder="Phone Number" id="pNumber" v-model="pNumber"><br>
+                <input type="text" placeholder="Email" id="eMail" v-model="eMail"><br>
                 <button @click="addFriend">Add</button>
             </form>
+            <hr>
         </div>
 
         <div class="container">
@@ -28,14 +31,23 @@
             return {
                 fName: '',
                 lName: '',
+                pNumber: '',
+                eMail: ''
             }
         },
         methods: {
             addFriend() {
                 this.$root.$data.contacts.push({
                     first_name: this.fName,
-                    last_name: this.lName
+                    last_name: this.lName,
+                    phone_number: this.pNumber,
+                    email: this.eMail
                 });
+
+                this.fName = '';
+                this.lName = '';
+                this.pNumber = '';
+                this.eMail = '';
             }
         },
         computed: {
